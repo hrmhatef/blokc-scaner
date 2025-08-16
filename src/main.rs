1f7e2c6eb6fdc7da0240d800b1c489f222207aaa
hatef <rahmani.hatef@gmail.com>
2025-08-16 18:00:32 +0300

    Add a hello zama app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod cmd;
+mod error;
+mod config;
+
+use std::{path::PathBuf, str::FromStr};
+
+use env_logger::Builder;
+use log::LevelFilter;
+
+#[tokio::main]
+async fn main() -> config::AppResult<()> {
+    let args = cmd::parse();
+    let cfg = config::load(PathBuf::from(args.config_path))?;
+
+    let level = LevelFilter::from_str(&cfg.log.level).expect("failed to parse the log level");
+
+    let _logger = Builder::new()
+        .filter_level(level)
+        .format_timestamp_secs()
+        .init();
+
+    log::info!("hello zama");
+
+    Ok(())
 }
